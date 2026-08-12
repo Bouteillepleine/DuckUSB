45f187554b4be1fe2041068b5b921d84408c7d73
YxxX <steve0007.xda@gmail.com>
2026-08-12 21:26:28 +0200

    Declare System Framework xposedscope so framework mode loads into system_server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.strawing.duckusb"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.2.1"
         vectorDrawables { useSupportLibrary = true }
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
